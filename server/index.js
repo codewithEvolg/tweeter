5c4dce498e22c018c67039a76a12b126c0493fb4
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-06-15 23:33:50 -0400

    UI design including tweet text area

--- server/index.js
+++ server/index.js
@@ -6,13 +6,8 @@ const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
 // The in-memory database of tweets. It's a basic object with an array in it.
 const db = require("./lib/in-memory-db");
-
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
 // actual database it uses and see little to no changes elsewhere in the code
@@ -21,7 +16,6 @@ const db = require("./lib/in-memory-db");
 // Because it exports a function that expects the `db` as a parameter, we can
 // require it and pass the `db` parameter immediately:
 const DataHelpers = require("./lib/data-helpers.js")(db);
-
 // Update the dates for the initial tweets (data-files/initial-tweets.json).
 require("./lib/date-adjust")();
 
@@ -29,6 +23,9 @@ require("./lib/date-adjust")();
 // so it can define routes that use it to interact with the data layer.
 const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
 
