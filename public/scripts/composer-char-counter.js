d4c9d497a7db8d6aba085c014654a43b5eb8053e
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-06-16 22:52:44 -0400

    added counter functionality using jquery and let counter turn red when below 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  $('#tweet-text').on('keyup', function() {
-    console.log(this);
+  $('#tweet-text').on('input', function() {
+    let maxLength = 140;
+    let textLength = $(this).val().length;
+    let remainingSpace = maxLength - textLength;
+    $(this).siblings('div').find('.counter').text(remainingSpace);
+    if (remainingSpace <= 0) {
+      $(this).siblings('div').find('.counter').css('color', 'red');
+    } else {
+      $(this).siblings('div').find('.counter').css('color', '');
+    }
   });
 });
+
