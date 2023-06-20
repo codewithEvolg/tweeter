c945d983457d9a8edb68c2baf60d9e47d163e428
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-06-19 22:53:39 -0400

    added the tweet container css design

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
     let maxLength = 140;
     let textLength = $(this).val().length;
     let remainingSpace = maxLength - textLength;
-    $(this).siblings('div').find('.counter').text(remainingSpace);
+    $(this).siblings('div').find('#counter').text(remainingSpace);
     if (remainingSpace <= 0) {
-      $(this).siblings('div').find('.counter').css('color', 'red');
+      $(this).siblings('div').find('#counter').css('color', 'red');
     } else {
-      $(this).siblings('div').find('.counter').css('color', '');
+      $(this).siblings('div').find('#counter').css('color', '');
     }
   });
 });
